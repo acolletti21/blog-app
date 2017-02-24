15af9325495e8bca930b7dc9c0b56affe54f114e
acolletti21 <ashleymcolletti@gmail.com>
2017-02-23 23:35:19 -0600

    routes, models, controllers

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+  
+  get '/signup' => 'users#new'
+  get '/users' => 'users#create'
+
+#
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+#
+  get '/posts' => 'posts#index'
+
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
+
+  get '/posts/:id' => 'posts#show'
+
+  get 'posts/:id/edit' => 'posts#edit'
+  patch 'posts/:id' => 'posts#update'
+
+  delete ='posts/:id' => 'posts#destroy'
+
+#
+  get '/categories' => 'categories#index'
+
+  get '/categories/new' => 'categories#new'
+  post '/categories' => 'categories#create'
+
+  get '/categories/:id' => 'categories#show'
+
+end
